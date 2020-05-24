057be8c39cf56b9fcbca1e4b2638cd56fb6f85a0
Bregandert <dmitriy.bykov@mail.ru>
2020-05-24 18:21:21 +0300

    lesson_4

---
+++ lesson4/Lesson4.java
@@ -0,0 +1,57 @@
+package ru.geekbrains.java3.lesson4;
+
+public class Lesson4 {
+    static Object monitor=new Object();
+    static volatile char currentChar='A';
+
+    public static void main (String[] args){
+        new Thread(()->{
+            try {
+                for (int i=1; i<5;i++){
+                    synchronized (monitor){
+                        while (currentChar!='A'){
+                            monitor.wait();
+                        }
+                        System.out.println('A');
+                        currentChar='B';
+                        monitor.notifyAll();
+                    }
+                }
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }).start();
+        new Thread(()->{
+            try {
+                for (int i=1; i<5;i++){
+                    synchronized (monitor){
+                        while (currentChar!='B'){
+                            monitor.wait();
+                        }
+                        System.out.println('B');
+                        currentChar='C';
+                        monitor.notifyAll();
+                    }
+                }
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }).start();
+        new Thread(()->{
+            try {
+                for (int i=1; i<5;i++){
+                    synchronized (monitor){
+                        while (currentChar!='C'){
+                            monitor.wait();
+                        }
+                        System.out.println('C');
+                        currentChar='A';
+                        monitor.notifyAll();
+                    }
+                }
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }).start();
+    }
+}
