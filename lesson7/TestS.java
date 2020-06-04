514baaef585dc427766d0c9a6d04b12a9abb8aaf
Bregandert <dmitriy.bykov@mail.ru>
2020-06-04 12:59:07 +0300

    lesson7

---
+++ lesson7/TestS.java
@@ -0,0 +1,30 @@
+package Lesson7;
+
+public class TestS {
+
+        @Test
+        public static void method1(){
+            System.out.println("Метод1");
+        }
+
+        @BeforeSuite
+        public static void method2(){
+            System.out.println("Метод2-Before");
+        }
+
+        @AfterSuite
+        public static void method3(){
+            System.out.println("Метод3-After");
+        }
+
+        @Test(priority = 2)
+        public static void method4(){
+            System.out.println("Метод4");
+        }
+
+        @Test(priority = 10)
+        public static void method5() {
+            System.out.println("Метод5");
+        }
+    }
+
